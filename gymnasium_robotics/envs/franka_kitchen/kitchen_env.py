e998114b32647b2f60cd6223b4e1d93f37b61042
Hojoon Lee <joonleesky90@gmail.com>
2025-01-19 23:30:35 +0900

    Update kitchen_env.py

--- gymnasium_robotics/envs/franka_kitchen/kitchen_env.py
+++ gymnasium_robotics/envs/franka_kitchen/kitchen_env.py
@@ -279,10 +279,10 @@ class KitchenEnv(GoalEnv, EzPickle):
             ]
         )
 
-        self.robot_env.init_qpos += (
+        self.robot_env.init_qpos[:9] += (
             robot_init_qpos_random_ratio
-            * self.robot_env.robot_pos_noise_amp[8:]
-            * self.robot_env.np_random.uniform(low=-1.0, high=1.0, size=self.robot_env.init_qpos.shape)
+            * self.robot_env.robot_pos_noise_amp[:9]
+            * self.robot_env.np_random.uniform(low=-1.0, high=1.0, size=self.robot_env.init_qpos[:9].shape)
         )
 
         self.model = self.robot_env.model
